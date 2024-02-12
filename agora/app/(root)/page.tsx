d5d4d5d822000008bec41f532f3c100998e99315
qweku2000 <joeadofonyarko@gmail.com>
2024-02-12 21:42:26 +0000

    store and admin modals giving problems

---
+++ agora/app/(root)/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { UserButton } from "@clerk/nextjs";
+import {Modal} from "@/components/ui/modal"
+ 
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Modal title="Test" description="Test Desc" isOpen onClose={()=>{}} >
+          Children
+      </Modal>
+    </div>
+  )
+}
+
+export default SetupPage;
---
+++ agora/components/ui/modal.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Dialog, 
+        DialogContent, 
+        DialogDescription, 
+        DialogHeader, 
+        DialogTitle 
+    } from "@/components/ui/dialog";
+
+
+interface ModalProps {
+    title: string;
+    description: string;
+    isOpen:boolean;
+    children?: React.ReactNode;
+    onClose(): void;
+};
+
+export const Modal: React.FC<ModalProps> = ({title,description,isOpen=false,children,onClose}) =>{
+    const onChange = (open:boolean) => {
+        if (!open){
+            onClose();
+        }
+    };
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange} >
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
